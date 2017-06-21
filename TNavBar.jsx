cee069e792cc4ddd81ed4562e96abe7321c1ee04
Bino Varghese <binovarg@cisco.com>
2017-06-21 10:10:37 +0530

    test ci/cd

--- TNavBar.jsx
+++ TNavBar.jsx
@@ -137,7 +137,7 @@ onHomeSelect() {
 			<Navbar inverse collapseOnSelect fixedTop>
 				<Navbar.Header  >
 				  <Navbar.Brand >
-					<a href="#"  onClick={this.onHomeSelect}>CloudTest25GB</a>
+					<a href="#"  onClick={this.onHomeSelect}>JCloudTest25GB</a>
 				  </Navbar.Brand>
 				  <Navbar.Toggle />
 				</Navbar.Header>
